5cf236b6308103f017ee426545c7a820d5a1c862
chryzller john suing <mcjsuing@tip.edu.ph>
2026-02-12 17:49:46 +0800

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import EnrollmentForm from "./EnrollmentForm"; // import the form component
-import "./App.css"; // import your classy CSS styling
+import EnrollmentForm from "./EnrollmentForm";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1 style={{ textAlign: "center", margin: "20px 0", color: "#004085" }}>
-          ADEi University Digital Registrar
-        </h1>
-        <p style={{ textAlign: "center", color: "#6c757d" }}>
-          Student Enrollment Portal
-        </p>
-      </header>
-
-      {/* Render the Enrollment Form */}
+      <h1 style={{ textAlign: "center", color: "#004085" }}>
+        ADEi University Digital Registrar
+      </h1>
       <EnrollmentForm />
     </div>
   );
